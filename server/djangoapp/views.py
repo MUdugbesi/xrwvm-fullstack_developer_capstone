ddf472a6c3bdf95c466043e91a8126a1f035c276
Marvelous <marvelous.udugbesi@icloud.com>
2026-04-10 21:05:30 +0100

    added car make and models, populate cars

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,6 +1,7 @@
 # Uncomment the required imports before adding the code
 
-# from django.shortcuts import render
+from .models import CarModel, CarMake
+from django.shortcuts import render
 # from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 # from django.shortcuts import get_object_or_404, render, redirect
@@ -12,7 +13,7 @@ from django.contrib.auth import login, authenticate, logout
 import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
-# from .populate import initiate
+from .populate import initiate
 
 
 # Get an instance of a logger
@@ -75,9 +76,8 @@ def registration(request):
 
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
-# def get_dealerships(request):
-# ...
-
+def get_dealerships(request):
+    pass
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
 # def get_dealer_reviews(request,dealer_id):
 # ...
@@ -89,3 +89,19 @@ def registration(request):
 # Create a `add_review` view to submit a review
 # def add_review(request):
 # ...
+
+
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(count)
+    if count == 0:
+        initiate()
+    
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+
+    for car_model in car_models:
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    
+    return JsonResponse({"CarModels":cars})
+
